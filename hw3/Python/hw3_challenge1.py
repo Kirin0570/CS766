9b82d09ca88e43cb0f9f0f4633c80a2004643d45
Kirin <qli478@wisc.edu>
2024-03-01 19:45:55 -0600

    Update 1d - v1

--- hw3/Python/hw3_challenge1.py
+++ hw3/Python/hw3_challenge1.py
@@ -78,4 +78,56 @@ def lineSegmentFinder(orig_img: np.ndarray, edge_img: np.ndarray, hough_img: np.
     Returns:
         line_segement_img: PIL image with line segments drawn.
     '''
-    raise NotImplementedError
+    hough_peaks = np.where(hough_img > hough_threshold)
+
+    line_segment_image = Image.fromarray(orig_img.astype(np.uint8)).convert('RGB')
+    draw = ImageDraw.Draw(line_segment_image) 
+
+    theta_num_bins, rho_num_bins = hough_img.shape
+    nrow, ncol = orig_img.shape
+    diag_len = np.sqrt(nrow**2 + ncol**2)
+
+    for i, j in zip(*hough_peaks):
+        theta = i * np.pi/theta_num_bins
+        rho = -diag_len + j * (2 * diag_len/rho_num_bins)
+        xp0 = 0 
+        while xp0 < nrow:
+            yp0 = (rho + xp0 * np.sin(theta))/np.cos(theta)
+            xp1 = xp0
+            flag_start = False    # flag for starting point
+            # check if (xp0, yp0) is a starting point 
+            for k in range(-2, 2):
+                for l in range(-2, 2):
+                    xp = xp0 + k; yp = yp0 + l
+                    if xp >=0 and xp < nrow and yp >= 0 and yp < ncol and edge_img[xp, yp] > 0:
+                        flag_start = True
+                        break
+            if flag_start:
+                # (xp0, yp0) is a starting point
+                # search for ending point
+                xp += 1
+                while xp1 < nrow:
+                    yp1 = (rho + xp1 * np.sin(theta))/np.cos(theta)
+                    flag_end = True    # flag for ending point
+                    # check if (xp1, yp1) is a ending point 
+                    for k in range(-2, 2):
+                        for l in range(-2, 2):
+                            xp = xp1 + k; yp = yp1 + l
+                            if xp >=0 and xp < nrow and yp >= 0 and yp < ncol and edge_img[xp, yp] > 0:
+                                flag_end = False
+                                break
+                    if flag_end:
+                        break
+                    else:
+                        xp += 1
+
+                # draw a line segment
+                xp1 -= 1
+                yp1 = (rho + xp1 * np.sin(theta))/np.cos(theta)
+                draw.line((yp0, xp0, yp1, xp1), fill=128, width=5)
+
+            # move on
+            xp0 = xp1 + 1
+        
+    line_segment_image.show()
+    return  line_segment_image
